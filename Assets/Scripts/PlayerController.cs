53337e707b05553d7893315db19b64fd41124ee3
Pedro Abreu <pedromiguelrijodeabreu@gmail.com>
2024-10-22 22:29:19 +0100

    Started camera boundaries

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Cinemachine;
 using System.Threading.Tasks;
 
 public class PlayerController : MonoBehaviour
@@ -18,11 +19,34 @@ public class PlayerController : MonoBehaviour
 
     public Animator animator;
 
-    public Vector2 startingPosition = new Vector2(7.4f, -5f);//-10.5f, 0.5f);
+    public Vector2 startingPosition = new Vector2(7.4f, -5f);
+
+    public CinemachineConfiner cinemachineConfiner;
 
     private void Start()
     {
         transform.position = startingPosition;
+
+        if (cinemachineConfiner == null)
+        {
+            GameObject virtualCameraObject = GameObject.FindGameObjectWithTag("VirtualCamera");
+
+            if (virtualCameraObject != null)
+            {
+                cinemachineConfiner = virtualCameraObject.GetComponent<CinemachineConfiner>();
+
+                if (cinemachineConfiner == null)
+                {
+                    Debug.LogError("CinemachineConfiner not found on the virtual camera object.");
+                }
+            }
+            else
+            {
+                Debug.LogError("GameObject with tag 'VirtualCamera' not found in the scene.");
+            }
+        }
+
+        UpdateConfiner();
     }
 
     private void Update()
@@ -63,90 +87,116 @@ public class PlayerController : MonoBehaviour
 
                 startingPosition = new Vector2(-7.5f, -2);
                 ChangePosition();
+                UpdateConfiner();
                 break;
             case "ExitStartToHallway":
                 SceneManager.LoadScene("Scenes/Hallway");
 
                 startingPosition = new Vector2(-8.5f, -0.25f);
                 ChangePosition();
+                UpdateConfiner();
                 break;
             case "ExitHallwayToStart":
                 SceneManager.LoadScene("Scenes/Start");
 
                 startingPosition = new Vector2(8.5f, -0.25f);
                 ChangePosition();
+                UpdateConfiner();
                 break;
             case "ExitHallwayToSewerStart":
                 SceneManager.LoadScene("Scenes/SewerStart");
 
                 startingPosition = new Vector2(-8.5f, -0.25f);
                 ChangePosition();
+                UpdateConfiner();
                 break;
             case "ExitSewerStartToHallway":
                 SceneManager.LoadScene("Scenes/Hallway");
 
                 startingPosition = new Vector2(8.5f, -0.25f);
                 ChangePosition();
+                UpdateConfiner();
                 break;
             case "ExitSewerStartToSewerStart2":
                 SceneManager.LoadScene("Scenes/SewerStart2");
 
                 startingPosition = new Vector2(-8.5f, -1f);
                 ChangePosition();
+                UpdateConfiner();
                 break;
             case "ExitSewerStartToEndStart":
                 SceneManager.LoadScene("Scenes/EndStart");
 
                 startingPosition = new Vector2(6.5f, -3f);
                 ChangePosition();
+                UpdateConfiner();
                 break;
             case "ExitEndStartToSewerStart":
                 SceneManager.LoadScene("Scenes/SewerStart");
 
                 startingPosition = new Vector2(-6.5f, -3f);
                 ChangePosition();
+                UpdateConfiner();
                 break;
             case "ExitEndStartToEnd":
                 SceneManager.LoadScene("Scenes/End");
 
                 startingPosition = new Vector2(3f, 3f);
                 ChangePosition();
+                UpdateConfiner();
                 break;
             case "ExitEndToEndStart":
                 SceneManager.LoadScene("Scenes/EndStart");
 
                 startingPosition = new Vector2(-5.5f, 1f);
                 ChangePosition();
+                UpdateConfiner();
                 break;
             case "ExitSewerStart2ToSewerStart":
                 SceneManager.LoadScene("Scenes/SewerStart");
 
                 startingPosition = new Vector2(8.5f, -1.5f);
                 ChangePosition();
+                UpdateConfiner();
                 break;
             case "ExitSewerStart2ToSewerFlood":
                 SceneManager.LoadScene("Scenes/SewerFlood");
 
                 startingPosition = new Vector2(4.5f, 4f);
                 ChangePosition();
+                UpdateConfiner();
                 break;
             case "ExitSewerFloodToSewerStart2":
                 SceneManager.LoadScene("Scenes/SewerStart2");
 
                 startingPosition = new Vector2(6.5f, -4f);
                 ChangePosition();
+                UpdateConfiner();
                 break;
             case "ExitSecurityRoomToStart":
                 SceneManager.LoadScene("Scenes/Start");
 
                 startingPosition = new Vector2(-8.5f, -5.25f);
                 ChangePosition();
+                UpdateConfiner();
                 break;
         }
     }
     void ChangePosition()
     {
         transform.position = startingPosition;
+    }
+
+    void UpdateConfiner()
+    {
+        PolygonCollider2D newConfinerShape = GameObject.FindObjectOfType<PolygonCollider2D>();
+
+        if (newConfinerShape != null && cinemachineConfiner != null)
+        {
+            cinemachineConfiner.m_BoundingShape2D = newConfinerShape;
+
+            cinemachineConfiner.InvalidatePathCache();
+        }
     }
 
 }
